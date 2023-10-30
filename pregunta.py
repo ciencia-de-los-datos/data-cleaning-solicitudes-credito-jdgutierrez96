8eafcec52dfde6d7f239c7dd9af04d146f5f5dbc
jdgutierrez96 <jdgutierreze@unal.edu.co>
2023-10-30 07:52:49 -0500

    Correct tests.py and update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,42 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
-import pandas as pd
 
+import pandas as pd
+import numpy as np
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv('solicitudes_credito.csv', sep = ';')
 
-    #
-    # Inserte su código aquí
-    #
+    df = df.dropna()
+
+    df = df.drop(columns = ['Unnamed: 0'])
+
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(np.int64)
+
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], format = 'mixed', dayfirst = True)
+
+    df['monto_del_credito'] = df['monto_del_credito'].str.split('.').str[0]
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(r'\D', '', regex = True)
+    df['monto_del_credito'] = df['monto_del_credito'].astype(np.int64)
+
+    string_columns = ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'barrio', 'línea_credito']
+    replace_dict = {
+        'á': 'a',
+        'é': 'e',
+        'í': 'i',
+        'ó': 'o',
+        'ú': 'u',
+        'ü': 'u',
+        '-': ' ',
+        '_': ' '
+    }
+
+    for string_column in string_columns:
+        for key in replace_dict:
+            df[string_column] = df[string_column].str.lower().str.replace(key, replace_dict[key])
+
+    df = df.drop_duplicates()
 
     return df
